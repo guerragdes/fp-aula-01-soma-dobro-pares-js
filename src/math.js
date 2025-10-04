9205f9e2ad6a597754ab5a1ad2a57e02424e6388
João Paulo <jotasage@gmail.com>
2025-10-04 12:39:39 -0300

    Implementação dos códigos funcional e estruturado. Pequena alteração no vetor const numeros.

--- src/math.js
+++ src/math.js
@@ -8,7 +8,13 @@
  * @param {Array} numeros vetor com os números desejados
  */
 function somaDobroNumerosParesFuncional(numeros){
-    // TODO: Insira seu código funcional aqui
+    const somaQuadradoPares =
+        numeros
+            .filter(n => n % 2 === 0)
+            .map(n => n * 2)
+            .reduce((soma, n) => soma + n, 0);
+
+    return somaQuadradoPares;
 }
 
 /**
@@ -17,7 +23,15 @@ function somaDobroNumerosParesFuncional(numeros){
  * @param {Array} numeros vetor com os números desejados
  */
 function somaDobroNumerosParesProcedural(numeros){
-    // TODO: Insira seu código estruturado aqui   
+    let somaQuadradoPares = 0;
+
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] % 2 === 0) {
+            somaQuadradoPares += numeros[i] * 2;
+        }
+    }
+
+    return somaQuadradoPares;
 }
 
 export { somaDobroNumerosParesFuncional, somaDobroNumerosParesProcedural };
